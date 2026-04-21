9d29b42c13eca1c6399c89f689e223e581473630
venkatapushkalpolepalli-debug <venkatapushkalpolepalli@gmail.com>
2026-04-21 15:40:15 +0530

    Acceptuserslotinput

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,31 +1,27 @@
-import java.util.Random;
+import java.util.Scanner;
 
-class Toss {
-    public static void main(String[] args) {
-        Random random = new Random();
+class InputHandler {
 
-        int tossResult = random.nextInt(2);
+    public static int getSlot() {
+        Scanner sc = new Scanner(System.in);
+        int slot;
 
-        String player1 = "Player 1";
-        String player2 = "Player 2";
-
-        String currentPlayer;
-        char player1Symbol;
-        char player2Symbol;
-
-        if (tossResult == 0) {
-            currentPlayer = player1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            currentPlayer = player2;
-            player2Symbol = 'X';
-            player1Symbol = 'O';
+        while (true) {
+            System.out.print("Enter slot (1-9): ");
+            if (sc.hasNextInt()) {
+                slot = sc.nextInt();
+                if (slot >= 1 && slot <= 9) {
+                    return slot;
+                }
+            } else {
+                sc.next();
+            }
+            System.out.println("Invalid input. Try again.");
         }
+    }
 
-        System.out.println("Toss Result: " + (tossResult == 0 ? player1 : player2) + " starts");
-        System.out.println(player1 + " Symbol: " + player1Symbol);
-        System.out.println(player2 + " Symbol: " + player2Symbol);
-        System.out.println("Current Player: " + currentPlayer);
+    public static void main(String[] args) {
+        int chosenSlot = getSlot();
+        System.out.println("You selected: " + chosenSlot);
     }
 }

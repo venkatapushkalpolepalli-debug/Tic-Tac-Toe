a47e0271049124924fd3a49412d714dda7be6abf
venkatapushkalpolepalli-debug <venkatapushkalpolepalli@gmail.com>
2026-04-21 14:54:57 +0530

    printing empty tictactoe

--- src/main/java/tictactoe.java
+++ src/main/java/tictactoe.java
@@ -1,5 +1,23 @@
 public class tictactoe {
-    public static void main(String[] args){
 
+
+        public static void main(String[] args) {
+
+            char[][] board = new char[3][3];
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    board[i][j] = '-';
+                }
+            }
+
+            System.out.println("Initial Tic-Tac-Toe Board:");
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
     }
-}
